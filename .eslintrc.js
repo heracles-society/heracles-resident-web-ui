0480d1e2fc8f3e5cbc48ea533c7e0cfd5850c270
Avik <sayavik@gmail.com>
2020-05-07 13:36:05 +0530

    Initial commit after scafollding

---
+++ .eslintrc.js
@@ -0,0 +1,72 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'google',
+    'prettier',
+    'prettier/react',
+    'react-app',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+  ],
+  plugins: ['react', 'prettier', 'react-hooks', 'jsx-a11y', 'import'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'require-jsdoc': 0,
+    'prettier/prettier': 'error',
+    'no-invalid-this': 0,
+    'react/prop-types': 0,
+    'react-hooks/rules-of-hooks': 'warn',
+    'react-hooks/exhaustive-deps': 'error',
+    'import/no-unresolved': [2, {commonjs: true, amd: true}],
+    'react/self-closing-comp': [
+      'error',
+      {
+        component: true,
+        html: true,
+      },
+    ],
+    'import/named': 2,
+    'import/default': 2,
+    'import/export': 2,
+    'import/order': [
+      2,
+      {
+        groups: [
+          'external',
+          'builtin',
+          'index',
+          'sibling',
+          'parent',
+          'internal',
+        ],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
