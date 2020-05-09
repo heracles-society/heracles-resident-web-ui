22327de938632591d0af7402373886f09af5a052
Avik <sayavik@gmail.com>
2020-05-10 03:53:25 +0530

    disabled eslint and added container in the select society view

--- src/views/SelectSociety/SelectSociety.js
+++ src/views/SelectSociety/SelectSociety.js
@@ -1,4 +1,4 @@
-import {Grid, Box, Paper, Button} from '@material-ui/core';
+import {Grid, Box, Paper, Button, Container} from '@material-ui/core';
 import React from 'react';
 
 import Map from '../../components/Map';
@@ -13,87 +13,92 @@ const imageStyles = {
 
 export const SelectSociety = props => {
   return (
-    <Grid
-      container
-      alignItems="stretch"
-      direction="row"
-      style={{height: '100%'}}
-    >
-      <Grid item style={{padding: '32px'}} sm={9}>
-        <Grid container direction="row" alignItems="stretch" spacing={2}>
-          <Grid item container direction="column" alignItems="stretch" sm={8}>
-            <Grid item container sm spacing={2}>
-              <Grid item sm={3}>
-                <img
-                  style={{...imageStyles, marginTop: 0}}
-                  src={Image1}
-                  alt="test"
-                  title="test 1"
-                />
-                <img
-                  style={{...imageStyles, marginTop: 12}}
-                  src={Image1}
-                  alt="test"
-                  title="test 1"
-                />
-                <img
-                  style={{...imageStyles, marginTop: 12}}
-                  src={Image1}
-                  alt="test"
-                  title="test 1"
-                />
-              </Grid>
-              <Grid item sm={9}>
-                <img
-                  style={{...imageStyles, height: 394}}
-                  src={Image1}
-                  alt="test"
-                  title="test 1"
-                />
+    <Container maxWidth="lg" style={{height: '100%'}}>
+      <Grid
+        container
+        alignItems="stretch"
+        direction="row"
+        style={{height: '100%'}}
+      >
+        <Grid item style={{padding: '32px'}} sm={9}>
+          <Grid container direction="row" alignItems="stretch" spacing={2}>
+            <Grid item container direction="column" alignItems="stretch" sm={8}>
+              <Grid item container sm spacing={2}>
+                <Grid item sm={3}>
+                  <img
+                    style={{...imageStyles, marginTop: 0}}
+                    src={Image1}
+                    alt="test"
+                    title="test 1"
+                  />
+                  <img
+                    style={{...imageStyles, marginTop: 12}}
+                    src={Image1}
+                    alt="test"
+                    title="test 1"
+                  />
+                  <img
+                    style={{...imageStyles, marginTop: 12}}
+                    src={Image1}
+                    alt="test"
+                    title="test 1"
+                  />
+                </Grid>
+                <Grid item sm={9}>
+                  <img
+                    style={{...imageStyles, height: 394}}
+                    src={Image1}
+                    alt="test"
+                    title="test 1"
+                  />
+                </Grid>
               </Grid>
             </Grid>
-          </Grid>
-          <Grid
-            item
-            container
-            direction="column"
-            alignItems="stretch"
-            sm={4}
-            style={{padding: '8px'}}
-          >
-            <Paper style={{height: '100%'}}>
-              <Grid
-                style={{height: '100%'}}
-                item
-                container
-                direction="column"
-                alignItems="stretch"
-                sm
-              >
-                <Box flexGrow={1} style={{padding: 24}} />
-                <Box
-                  style={{padding: 24, backgroundColor: '#e3e3e3'}}
-                  display="flex"
-                  flexDirection="column"
+            <Grid
+              item
+              container
+              direction="column"
+              alignItems="stretch"
+              sm={4}
+              style={{padding: '8px'}}
+            >
+              <Paper style={{height: '100%'}}>
+                <Grid
+                  style={{height: '100%'}}
+                  item
+                  container
+                  direction="column"
                   alignItems="stretch"
+                  sm
                 >
-                  <Button variant="contained" color="primary">
-                    Contact Manager
-                  </Button>
-                </Box>
-              </Grid>
-            </Paper>
+                  <Box flexGrow={1} style={{padding: 24}} />
+                  <Box
+                    style={{padding: 24, backgroundColor: '#e3e3e3'}}
+                    display="flex"
+                    flexDirection="column"
+                    alignItems="stretch"
+                  >
+                    <Button variant="contained" color="primary">
+                      Contact Manager
+                    </Button>
+                  </Box>
+                </Grid>
+              </Paper>
+            </Grid>
           </Grid>
         </Grid>
+        <Grid item container direction="column" alignItems="stretch" sm={3}>
+          <Box
+            display="flex"
+            style={{
+              height: '100%',
+              boxShadow: 'rgb(195, 195, 195) -2px 0px 10px 0px',
+            }}
+          >
+            <Map />
+          </Box>
+        </Grid>
       </Grid>
-      <Grid item container direction="column" alignItems="stretch" sm={3}>
-        <Box
-          display="flex"
-          style={{height: '100%', boxShadow: '3px 0px 8px 0px #020202'}}
-        >
-          <Map />
-        </Box>
-      </Grid>
-    </Grid>
+    </Container>
   );
 };
